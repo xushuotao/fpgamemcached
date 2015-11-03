fa1b3ae3dd45083becf77637159e05c5d463518c
xushuotao <xushuotao@gmail.com>
2015-11-02 20:11:03 -0500

    JNI

---
+++ bsv/Test_Bluecache_JNI/RequestThread.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+public class RequestThread extends Thread{
+    private int threadId;
+
+    private BluecacheClient client;
+
+    private int [] keylenArray;// = new int[numTests];
+    private int [] vallenArray;// = new int[numTests];
+    private byte[][] keyArray;// = new byte[numTests][];
+    private byte[][] valArray;// = new byte[numTests][];
+    private boolean[] successArray;// = new boolean[numTests];
+
+    private boolean doSet;
+    private int numTests;
+
+    public RequestThread(BluecacheClient client, int threadId,
+                         int [] keylenArray,
+                         int [] vallenArray,
+                         byte[][] keyArray,
+                         byte[][] valArray,
+                         boolean[] successArray,
+                         int numTests
+                         ){
+        this.threadId = threadId;
+        this.client = client;
+        
+        this.doSet = true;
+        this.keylenArray = keylenArray;
+        this.vallenArray = vallenArray;
+        this.keyArray = keyArray;
+        this.valArray = valArray;
+        this.successArray = successArray;
+
+        this.numTests = numTests;
+    }
+
+    public void setParameters(boolean doSet){
+        this.doSet = doSet;
+    }
+
+    public void run(){
+        int pageSz = 8192;
+        if ( doSet ) {
+            // keylenArray = new int[numTests];
+            // vallenArray = new int[numTests];
+            // keyArray = new byte[numTests][];
+            // valArray = new byte[numTests][];
+            // successArray = new boolean[numTests];
+
+            Random r = new Random();
+            int fails = 0;
+            
+            for ( int i = 0; i < numTests; i++ ){
+                int keylen = r.nextInt(255) + 1;
+                //int keylen = 64;
+                //int vallen = r.nextInt(8*pageSz-keylen-8)+1;//rand()%255+1;
+                int vallen = pageSz-keylen-8;//rand()%255+1;
+                //int vallen = 6*pageSz-keylen-8;//rand()%255+1;
+    
+                keylenArray[i] = keylen;
+                vallenArray[i] = vallen;
+
+                keyArray[i] = new byte[keylen];
+                r.nextBytes(keyArray[i]);
+    
+                valArray[i] = new byte[vallen];
+                r.nextBytes(valArray[i]);
+    
+                //successArray[i] = client.set(new String(keyArray[i]), new String(valArray[i]));
+                //System.out.format("Client %d sends request for set\n", client.clientId());
+                successArray[i] = client.set(keyArray[i], valArray[i]);
+                //System.out.format("Client %d receives response for set\n", client.clientId());
+                if ( !successArray[i] ) fails++;
+
+            }
+            //System.out.format("Client %d, set fails = %d\n", threadId, fails);
+        } else {
+            //System.out.println(keyArray.length);
+            int fails = 0;
+            double diff = 0;
+            for ( int i = 0; i < numTests; i++ ){
+                //byte[] retval = client.get(new String(keyArray[i]));
+                //System.out.format("Java:: Client %d sends request for get\n", client.clientId());
+                long time_start = System.nanoTime();
+                byte[] retval = client.get(keyArray[i]);
+                long time_end = System.nanoTime();
+                diff+=(time_end - time_start)/1e3;
+                // try{
+                //     Thread.sleep(100);
+                // } catch (Exception e){
+                //     e.printStackTrace(System.out);
+                // }
+                if ( retval == null ) fails++;
+                if ( successArray[i] ) {
+                    if ( Arrays.equals(retval, valArray[i])) {
+                        //System.out.format("Client %d, get %d succeeds\n", threadId, i);
+                        //System.exit(0);
+                    } else {
+                        System.out.format("Client %d, get %d fails\n", threadId, i);
+                        // for ( int j = 0; j < keyArray.length; j++){
+                        //     System.out.format("Retval[%d] = %x, keyArray[%d] = %x, match = %b\n", j, retval[j], j, keyArray[i][j], retval[j]==keyArray[i][j]);
+                        // }
+                    }
+                }
+            }
+
+
+            //            System.out.format("Client %d, get fails = %d, avg_latency = %fus\n", threadId, fails, diff/numTests);
+        }
+    }
+}
+
---
+++ bsv/Test_Bluecache_JNI/TestBluecache.java
@@ -0,0 +1,106 @@
+//import BluecacheClient;
+import java.util.*;
+
+
+public class TestBluecache {
+
+
+    public static void main(String [] args){
+
+        Vector<RequestThread> threads = new Vector<RequestThread>();
+        Vector<BluecacheClient> clients = new Vector<BluecacheClient>();
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.format("Enter number of concurrent request threads: ");
+        int threadcount = in.nextInt();
+        System.out.format("Enter number of tests per threads: ");
+        int numTests = in.nextInt();
+
+        
+        int [][] keylenArray = new int[threadcount][];
+        int [][] vallenArray = new int[threadcount][];
+        byte[][][] keyArray = new byte[threadcount][][];
+        byte[][][] valArray = new byte[threadcount][][];
+        boolean[][] successArray = new boolean[threadcount][];
+
+        for ( int i = 0; i < threadcount; i++ ){
+            keylenArray[i] = new int[numTests];
+            vallenArray[i] = new int[numTests];
+            keyArray[i] = new byte[numTests][];
+            valArray[i] = new byte[numTests][];
+            successArray[i] = new boolean[numTests];
+
+            BluecacheClient client = new BluecacheClient();
+            RequestThread clientThread = new RequestThread(client, i,
+                                                           keylenArray[i],
+                                                           vallenArray[i],
+                                                           keyArray[i],
+                                                           valArray[i],
+                                                           successArray[i],
+                                                           numTests);
+            threads.add(clientThread);
+            clients.add(client);
+        }
+        
+        long start_time = System.nanoTime();
+        for (RequestThread thread: threads) {
+            thread.start();
+        }
+        for (RequestThread thread: threads) {
+            try {
+                thread.join();
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        long end_time = System.nanoTime();
+        double diff = (end_time - start_time)/1e3;
+
+        System.out.format("Bluecache number of sets = %d, performed within %f\n", numTests*threadcount, diff);
+        System.out.format("Bluecache Statistics: Sets %f MRPS\n", (double)(numTests*threadcount)/diff);
+        clients.get(0).avgGetLatency();
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception ex){
+            ex.printStackTrace(System.out);
+        }
+
+
+        Vector<RequestThread> getthreads = new Vector<RequestThread>();
+        for ( int i = 0; i < threadcount; i++ ){
+            RequestThread clientThread = new RequestThread(clients.get(i), i,
+                                                           keylenArray[i],
+                                                           vallenArray[i],
+                                                           keyArray[i],
+                                                           valArray[i],
+                                                           successArray[i],
+                                                           numTests);
+            getthreads.add(clientThread);
+        }
+
+
+        for (RequestThread thread: getthreads) {
+            thread.setParameters(false);
+        }
+
+        start_time = System.nanoTime();
+        for (RequestThread thread: getthreads) {
+            thread.start();
+        }
+        for (RequestThread thread: getthreads) {
+            try {
+                thread.join();
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        end_time = System.nanoTime();
+        diff = (end_time - start_time)/1e3;
+        System.out.format("Bluecache number of gets = %d, performed within %f\n", numTests*threadcount, diff);
+        System.out.format("Bluecache Statistics: Gets %f MRPS\n", (double)(numTests*threadcount)/diff);
+
+        System.out.format("Average c Get latency = %f\n", clients.get(0).avgGetLatency());
+    }
+}
